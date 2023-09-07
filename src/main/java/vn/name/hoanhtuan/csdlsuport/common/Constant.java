d8d84d65e8239993cc79b3f415301f7459ed7f2e
anhtuana2k422001 <anhtuana2k422001@gmail.com>
2023-09-07 23:33:36 +0700

    clean code

--- src/main/java/vn/name/hoanhtuan/csdlsuport/common/Constant.java
+++ src/main/java/vn/name/hoanhtuan/csdlsuport/common/Constant.java
@@ -15,4 +15,11 @@ public class Constant {
     public static final String BAO_DONG_VALID = "Bao dong hop le: {}";
     public static final String LOG_ITEM_PTH = "Item PTH: {} - ";
     public static final String LOG_ITEM_BAO_DONG = "ItemChar Baodong: {} - ";
+    public static final String TIM_KHOA = "Dạng 1: Tìm Khóa";
+    public static final String TIM_PHU_TOI_THIEU= "Dạng 2: Tìm PTH tối thiểu";
+    public static final String TIM_DANG_CHUAN = "Dạng 3: Tìm Dạng chuẩn";
+    public static final String RESULT_DANG_CHUAN_1 = "Kết luận: Vậy lược đồ quan hệ chỉ đạt dạng chuẩn 1NF";
+    public static final String RESULT_DANG_CHUAN_2 = "Kết luận: Vậy Lược đồ quan hệ chỉ đạt dạng chuẩn 2NF";
+    public static final String RESULT_DANG_CHUAN_3 = "Kết luận: Vậy lược đồ quan hệ chỉ đạt dạng chuẩn 3NF";
+    public static final String RESULT_DANG_CHUAN_BC = "Kết luận: Đạt dạng chuẩn BCNF";
 }
